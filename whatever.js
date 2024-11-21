f8980258aa9d82865e9c9f47cf9f7fe6f1b177a4
Felice Tufo <info@felicetufo.com>
2024-11-21 01:21:34 +0100

    changes for pagespeed insights

--- whatever.js
+++ whatever.js
@@ -1,11 +1,11 @@
-function whatever() {
+async function whatever() {
     var s = '&commat;'
         , n = '&#105;&#110;&#102;&#111;'
         , k = '&#102;&#101;&#108;&#105;&#099;&#101;&#116;&#117;&#102;&#111;&period;&#099;&#111;&#109;'
         , e = n + s + k
         , l = '<a name="mail" href="mailto:{{spam@uce.gov}}">{{spam@uce.gov}}</a>'.replace(/{{.+?(}})/g, e)
     ;
-    document.write(l);
+    document.getElementById("whatever").innerHTML = l;
 }
 
 whatever();
